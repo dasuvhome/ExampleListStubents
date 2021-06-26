20f7cbdd391f099adf5ff96e1ad280e5c648ee69
DASuvorov <dasuvhome@yandex.rue>
2021-06-26 12:56:16 +0400

    delete hBox

--- src/sample/controller/Controller.java
+++ src/sample/controller/Controller.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
@@ -42,66 +43,61 @@ public class Controller {
 
     @FXML
     private Label lbl_info;
-        DB db = new DB();
+    DB db = new DB();
     List<Student> students = db.getStudents();
     int lengthListSrudent;
 
     @FXML
     void initialize() throws IOException {
-             lengthListSrudent = students.size();
-            for (int i = 0; i< lengthListSrudent; i++) {
-                refresList(i);
+        lengthListSrudent = students.size();
+        for (int i = 0; i < lengthListSrudent; i++) {
+            refresList(i);
+        }
+        btn_add_user.setOnAction(event -> {
+
+            String name = text_name.getCharacters().toString();
+            String surname = text_surname.getCharacters().toString();
+            int age = Integer.parseInt(text_age.getCharacters().toString());
+
+            Student student = new Student(name, surname, age);
+            System.out.println(student);
+            students.add(student);
+
+            try {
+                lengthListSrudent = students.size();
+                refresList(lengthListSrudent - 1);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-              btn_add_user.setOnAction(event ->{
-
-              String name  = text_name.getCharacters().toString();
-              String surname  = text_surname.getCharacters().toString();
-              int age  = Integer.parseInt(text_age.getCharacters().toString());
-
-                Student student = new Student(name, surname, age);
-               System.out.println(student);
-               students.add(student);
-
-                  try {
-                      lengthListSrudent = students.size();
-                      refresList(lengthListSrudent-1);
-                  } catch (IOException e) {
-                      e.printStackTrace();
-                  }
-              });
+        });
 
     }
+
     public void refresList(int lengthListSrudent) throws IOException {
         Node node = null;
 
-            node = FXMLLoader.load(getClass().getResource("/sample/scene/sceneStudent.fxml"));
-            Label name = (Label)node.lookup("#name");
-            Label surname = (Label)node.lookup("#surname");
-            Label age = (Label) node.lookup("#age");
-            name.setText(students.get(lengthListSrudent).getName());
-            surname.setText(students.get(lengthListSrudent).getSurname());
-            age.setText(""+ students.get(lengthListSrudent).getAge());
-            final Node nodeSet = node;
-            node.setOnMouseEntered(event-> {
-                nodeSet.setStyle("-fx-background-color: #707173");
-            });
-            nodeSet.setOnMouseExited(mouseEvent -> {
-                nodeSet.setStyle("-fx-background-color: #343434");
-            });
+        node = FXMLLoader.load(getClass().getResource("/sample/scene/sceneStudent.fxml"));
+        Label name = (Label) node.lookup("#name");
+        Label surname = (Label) node.lookup("#surname");
+        Label age = (Label) node.lookup("#age");
+        name.setText(students.get(lengthListSrudent).getName());
+        surname.setText(students.get(lengthListSrudent).getSurname());
+        age.setText("" + students.get(lengthListSrudent).getAge());
+        final Node nodeSet = node;
+        node.setOnMouseEntered(event -> {
+            nodeSet.setStyle("-fx-background-color: #707173");
+        });
+        nodeSet.setOnMouseExited(mouseEvent -> {
+            nodeSet.setStyle("-fx-background-color: #343434");
+        });
 
 
-
-            HBox hBox = new HBox();
-            hBox.getChildren().add(node);
-            hBox.setAlignment(Pos.BASELINE_CENTER);
-            paneVBox.getChildren().add(hBox);
-            paneVBox.setSpacing(10);
-
-            paneVBox.getChildren().add(node);
-
-        }
+        paneVBox.setSpacing(10);
+        paneVBox.getChildren().add(node);
 
     }
+
+}
 
 
 
